962f0aef82e0f1815cd85c27d80ee8a5e6ee5e88
koharker <koharker@gmail.com>
2015-11-30 19:26:32 -0800

    Update signature_pad.js

--- js/signature_pad.js
+++ js/signature_pad.js
@@ -93,6 +93,9 @@ var SignaturePad = (function (document) {
             self._ctx.drawImage(image, 0, 0, width, height);
         };
         this._isEmpty = false;
+        document.clipboardData.setData('image/png', 'image');
+//      .clipboardData.setData('text/html', '<b>Hello, world!</b>');
+        document.preventDefault(); // We want our data, not data from any selection, to be written to the clipboard
 /*        document.oncopy = function(event){
     			event.preventDefault();
     			event.clipboardData.setData("text", "これはテストです");
