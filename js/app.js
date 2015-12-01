5ef74fd5c6d90c8140be0ef3823d4f6368c219e5
koharker <koharker@gmail.com>
2015-11-30 19:54:30 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -41,7 +41,8 @@ document.addEventListener('copy', function(e){
            e.clipboardData.setData('image/png', 'image');
 //     e.clipboardData.setData('text/html', '<b>Hello, world!</b>');
            e.preventDefault(); // We want our data, not data from any selection, to be written to the clipboard
-    )}
+        )
+    }
 });
 
 saveButton.addEventListener("click", function (event) {
